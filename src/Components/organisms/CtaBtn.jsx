008a3b0671dfa9d438dd9facb85ec036a875b7a4
Gabriel Thankgod <155149161+Thankgodgab@users.noreply.github.com>
2025-11-06 15:48:25 +0100

    Update src/Components/organisms/CtaBtn.jsx
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- src/Components/organisms/CtaBtn.jsx
+++ src/Components/organisms/CtaBtn.jsx
@@ -1,9 +1,13 @@
 
-function CtaBtn({ text }) {
+function CtaBtn({ text, onClick, ...props }) {
     return (
-        <div className="py-[17px] px-[30px] text-[16px] text-primary font-semibold bg-gradient-to-r from-accent-secondary to-accent rounded-4xl hover:bg-gradient-to-r hover:from-accent hover:to-accent-secondary transition-colors duration-300 ease-in-out">
+        <button 
+            onClick={onClick}
+            {...props}
+            className="py-[17px] px-[30px] text-[16px] text-primary font-semibold bg-gradient-to-r from-accent-secondary to-accent rounded-4xl hover:bg-gradient-to-r hover:from-accent hover:to-accent-secondary transition-colors duration-300 ease-in-out"
+        >
             {text}
-        </div>
+        </button>
     )
 }
 
